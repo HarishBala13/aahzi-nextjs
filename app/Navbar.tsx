220802ec45a9959a314030741416f290a07de609
Harish-2023 <balahkharish@gmail.com>
2023-12-06 20:30:12 +0530

    commiting a new project solved nextjs routing and completed half of the form design commiting date :- 06/12/2023 @08:30P.M

---
+++ app/Navbar.tsx
@@ -0,0 +1,80 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-xl">
+      <div className="flex-none">
+        <button className="btn btn-square btn-ghost" title="bar">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block w-5 h-5 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </button>
+      </div>
+      <div className="flex-auto gap-6">
+        <div className="flex justify-evenly w-10/12">
+            <Link href={"/"} className="hover:text-rose-500">Aahzi</Link>
+            <Link href={"/form"} className="hover:text-rose-500">Form</Link>
+            <Link href={"/admin"} className="hover:text-rose-500">Admin</Link>
+            <Link href={"/login"} className="hover:text-rose-500">Login</Link>
+        </div>
+      </div>
+      <div className="flex-none gap-2">
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+            disabled={true}
+          />
+        </div>
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <Image
+                alt="Tailwind CSS Navbar component"
+                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                title="user-image"
+                width={200}
+                height={200}
+              ></Image>
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="mt-3 z-[1] p-4 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ app/form/page.tsx
@@ -0,0 +1,180 @@
+/* eslint-disable react/jsx-no-duplicate-props */
+"use client";
+import { useState, ChangeEvent } from "react";
+
+const ChatBotForm = () => {
+  const [mathsScore, setMathsScore] = useState<string>("");
+  const [physicsScore, setPhysicsScore] = useState<string>("");
+  const [chemistryScore, setChemistryScore] = useState<string>("");
+  const [cutoffMarks, setCutoffMarks] = useState<number | null>(null);
+  const [errors, setErrors] = useState<{ [key: string]: string }>({});
+  const [errorColors, setErrorColors] = useState<{ [key: string]: string }>({});
+
+  const handleMathsChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setMathsScore(event.target.value);
+    calculatecutoff();
+  };
+
+  const handlePhysicsChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPhysicsScore(event.target.value);
+    calculatecutoff();
+  };
+
+  const handleChemistryChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setChemistryScore(event.target.value);
+    calculatecutoff();
+  };
+
+  const calculatecutoff = () => {
+    setErrors({});
+    setErrorColors({});
+
+    const parseInput = (input: string, subject: string): number => {
+      const value = parseFloat(input) || 0;
+      if (value < 0 || value > 100 || isNaN(value)) {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          [subject]: "Invalid marks! Please enter a value between 0 and 100.",
+        }));
+        setErrorColors((prevColors) => ({
+          ...prevColors,
+          [subject]: "red",
+        }));
+        return 0;
+      }
+      return value;
+    };
+    const maths = parseInput(mathsScore, "maths");
+    const physics_currentscore = parseInput(physicsScore, "physics");
+    const chemistry_currentscore = parseInput(chemistryScore, "chemistry");
+
+    const physics = physics_currentscore / 2;
+    const chemistry = chemistry_currentscore / 2;
+    const cutoff = maths + physics + chemistry;
+
+    setCutoffMarks(parseFloat(cutoff.toFixed(2)));
+  };
+
+  return (
+    <div className="mt-6 grid place-items-center gap-4">
+      <p className="text-5xl">Form</p>
+      <form className="flex justify-center">
+        <div className="space-y-12">
+          <div>
+            <label
+              htmlFor=""
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Mobile Number
+            </label>
+            <input
+              type="tel"
+              title="mobilenumber"
+              maxLength={10}
+              placeholder="Enter your Mobile Number"
+              className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+
+          <div className="grid grid-cols-1 gap-x-2 gap-y-4 sm:grid-cols-6">
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="physics"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Physics
+              </label>
+              <div className="mt-2">
+                <input
+                  type="tel"
+                  name="physics"
+                  id="physics"
+                  placeholder="Enter marks"
+                  maxLength={3}
+                  value={physicsScore}
+                  onChange={handlePhysicsChange}
+                  style={{ borderColor: errorColors.physics || "" }}
+                  autoComplete="given-name"
+                  className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                {errors.physics && (
+                  <p
+                    className={"text-sm"}
+                    style={{ color: errorColors.physics || "" }}
+                  >
+                    {errors.physics}
+                  </p>
+                )}
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="chemistry"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Chemistry
+              </label>
+              <div className="mt-2">
+                <input
+                  type="tel"
+                  name="chemistry"
+                  id="chemistry"
+                  placeholder="Enter marks"
+                  maxLength={3}
+                  value={chemistryScore}
+                  onChange={handleChemistryChange}
+                  autoComplete="given-name"
+                  className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="maths"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Maths
+              </label>
+              <div className="mt-2">
+                <input
+                  type="tel"
+                  name="maths"
+                  id="maths"
+                  placeholder="Enter marks"
+                  maxLength={3}
+                  value={mathsScore}
+                  onChange={handleMathsChange}
+                  autoComplete="given-name"
+                  className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <h3>Cutoff Marks:</h3>
+              <p className="text-sm font-medium leading-6 text-gray-900">
+                {cutoffMarks}
+              </p>
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor=""
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Interested college
+            </label>
+            <input
+              type="text"
+              title="interestedcollege"
+              placeholder="Enter your Interested College"
+              className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        <div className="btn btn-info text-white"><button title="submit" type="submit">Submit</button></div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ChatBotForm;
